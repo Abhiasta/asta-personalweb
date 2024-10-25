f91d8b44ab9b295666e65e615cba1fa207462326
Abhiasta <abhiasta15@yahoo.com>
2024-10-25 11:10:05 +0700

    newest

--- Blog/blog-js/blog.js
+++ Blog/blog-js/blog.js
@@ -5,51 +5,58 @@ function addblog(event) {
 
   const inputprojectname = document.getElementById("project-name").value;
   const inputdescription = document.getElementById("desc").value;
-  //   const inputimage = document.getElementById("blogimage").value
+  const inputimage = document.getElementById("blogimage").files;
 
-  //   const image = URL.createObjectURL(blogimage[0]);
+  const image = URL.createObjectURL(inputimage[0]);
 
   const blog = {
     title: inputprojectname,
     description: inputdescription,
-    //   createdAt: new Date()
-    //   image: image,
+    createdAt: new Date(),
+    image: image,
   };
 
-  blogs.push(blog);
+  blogs.unshift(blog);
   renderblog();
-
-  function renderblog() {
-    let html = "";
-
-    for (let index = 0; index < blogs.lengt; index++) {
-      html += `<div>
-            <img class="blog-image" src="${blogs[index].image}" />
-          </div>
-          <div class="blog-content">
-            <h1 class="blogtitle">
-              <a href="blog-detail.html" target="_blank"
-              >${blogs[index].title}</a>
-            </h1>
-            <div class="detail-blog-content">
-              <div class="author">12 Jul 2021 22:30 WIB | Abhiasta Gustyanugra</div>
-            </div>
-            <p>
-            ${blogs[index].description}
-            </p>
-            <p>
-              
-            </p>
-            <div class="btn-group">
-              <button class="btnedit">Edit Post</button>
-              <button class="btndelete">Delete Post</button>
-            </div>`;
-    }
-    document.getElementById("content").innerHTML = html;
-  }
-  // /* <img src="${blogs[index].image}" alt="" /> */
-  // >${blogs[index].title}</a
-  // ${getFullTime(blogs[index].createdAt)}
-  // ${blogs[index].content}
   console.log(blogs);
 }
+function renderblog() {
+  let html = ``;
+
+  for (let index = 0; index < blogs.length; index++) {
+    html += `
+<div class="bloglist" >
+    <div>
+    <img class="blog-image" src="${blogs[index].image}" />
+  </div>
+  <div class="blog-content">
+    <h1 class="blogtitle">
+      <a href="blog-detail.html" target="_blank"
+      >${blogs[index].title}</a>
+    </h1>
+    <div class="detail-blog-content">
+      <div class="author">${blogs[index].createdAt} | Abhiasta Gustyanugra</div>
+    </div>
+    <p>
+    ${blogs[index].description}
+    </p>
+    <p>
+
+    </p>
+    <div class="btn-group">
+      <button class="btnedit">Edit Post</button>
+      <button class="btndelete" onclick="deleteblog(${index})" >Delete Post</button>
+    </div>
+  </div>
+</div>`;
+  }
+  document.getElementById("content").innerHTML = html;
+}
+function deleteblog(index) {
+  blogs.splice(index, 1);
+  renderblog();
+}
+// /* <img src="${blogs[index].image}" alt="" /> */
+// >${blogs[index].title}</a
+// ${getFullTime(blogs[index].createdAt)}
+// ${blogs[index].content}
---
+++ assets/js/nav.js
@@ -0,0 +1,4 @@
+function menutoggle() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("hide");
+}
